7cf61127a36b4b70f666e547b0151bc1a412bbb2
Marco Pivetta <ocramius@gmail.com>
2014-06-23 19:54:06 +0000

    Importing state as of zendframework/zf2@2a9c97db0b21336d85937caaec2ecda3de6dc474 (1403553246)
    Automatic import via rsync

--- PhpClassFile.php
+++ PhpClassFile.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -19,12 +19,7 @@ class PhpClassFile extends SplFileInfo
     /**
      * @var array
      */
-    protected $classes = array();
-
-    /**
-     * @var array
-     */
-    protected $namespaces = array();
+    protected $classes;
 
     /**
      * Get classes
@@ -37,39 +32,14 @@ class PhpClassFile extends SplFileInfo
     }
 
     /**
-     * Get namespaces
-     *
-     * @return array
-     */
-    public function getNamespaces()
-    {
-        return $this->namespaces;
-    }
-
-    /**
      * Add class
      *
      * @param  string $class
-     * @return self
+     * @return PhpClassFile
      */
     public function addClass($class)
     {
         $this->classes[] = $class;
-        return $this;
-    }
-
-    /**
-     * Add namespace
-     *
-     * @param  string $namespace
-     * @return self
-     */
-    public function addNamespace($namespace)
-    {
-        if (in_array($namespace, $this->namespaces)) {
-            return $this;
-        }
-        $this->namespaces[] = $namespace;
         return $this;
     }
 }
