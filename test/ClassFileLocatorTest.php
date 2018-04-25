c0131ef0fb56966c7d41850d86ce396d1c4cdcb2
Matthew Weier O'Phinney <matthew@zend.com>
2018-04-25 11:03:04 -0500

    Update license docblocks and RTH styling

--- test/ClassFileLocatorTest.php
+++ test/ClassFileLocatorTest.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/zendframework/zend-file for the canonical source repository
+ * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-file/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendTest\File;
@@ -14,11 +12,6 @@ use Zend\File\ClassFileLocator;
 use Zend\File\Exception;
 use Zend\File\PhpClassFile;
 
-/**
- * Test class for Zend\File\ClassFileLocator
- *
- * @group      Zend_File
- */
 class ClassFileLocatorTest extends TestCase
 {
     public function testConstructorThrowsInvalidArgumentExceptionForInvalidStringDirectory()
--- test/TestAsset/WithMethodsNamedAfterKeywords/WithoutReturnTypeDeclaration.php
+++ test/TestAsset/WithMethodsNamedAfterKeywords/WithoutReturnTypeDeclaration.php
@@ -1,10 +1,7 @@
 <?php
-
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      https://github.com/zendframework/zend-file for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @see       https://github.com/zendframework/zend-file for the canonical source repository
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-file/blob/master/LICENSE.md New BSD License
  */
 
